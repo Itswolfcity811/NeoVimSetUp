e9bdcc4987d9924d48ed09ff835ebc27f348d34b
Itswolfcity811 <91641161+Itswolfcity811@users.noreply.github.com>
2024-12-04 09:59:02 -0600

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,2 +1,23 @@
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+mkdir ~/.config/nvim/
+
+cd ~/.config/nvim/
+
+touch init.vim
+
+VAR=$(cat <<EOF
+call plug#begin()
+
+" List your plugins here
+Plug 'theblob42/drex.nvim'
+Plug 'kyazdani42/nvim-web-devicons' " optional
+
+Plug 'lervag/vimtex'
+Plug 'lervag/vimtex', { 'tag': 'v2.15' }
+
+call plug#end()
+EOF
+
+echo $VAR >> init.vim
