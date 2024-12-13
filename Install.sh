b84eb07e47e3b55716539d3d242fc2f3cae0645b
Itswolfcity811 <91641161+Itswolfcity811@users.noreply.github.com>
2024-12-13 10:04:44 -0600

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -5,124 +5,148 @@ mkdir ~/.config/nvim/
 
 cd ~/.config/nvim/
 
-touch init.vim
+touch init.lua
 
 pip3 install pynvim
 pip3 install --upgrade pynvim
 
-cat > init.vim << EOF
-call plug#begin()
+cat > init.lua << EOF
+local vim = vim
+local Plug = vim.fn['plug#']
 
-" List your plugins here
-Plug 'theblob42/drex.nvim'
-Plug 'kyazdani42/nvim-web-devicons' " optional
+vim.call('plug#begin')
 
-Plug 'lervag/vimtex'
-Plug 'lervag/vimtex', { 'tag': 'v2.15' }
+-- List your plugins here
+Plug('theblob42/drex.nvim')
+Plug('kyazdani42/nvim-web-devicons') -- optional
 
-Plug 'puremourning/vimspector'
-Plug 'mfussenegger/nvim-jdtls'
-Plug 'elementdavv/vimspector-jdtls'
+Plug("'ryleelyman/latex.nvim'")
 
-function! UpdateRemotePlugins(...)
-	" Needed to refresh runtime files
-	let &rtp=&rtp
-	UpdateRemotePlugins
-  endfunction
+Plug('puremourning/vimspector')
+Plug('mfussenegger/nvim-jdtls')
+Plug('elementdavv/vimspector-jdtls')
 
-Plug 'gelguy/wilder.nvim', { 'do': function('UpdateRemotePlugins') }
+function UpdateRemotePlugins(...)
+    -- Needed to refresh runtime files
+    --let &rtp=&rtp
+    vim.cmd("UpdateRemotePlugins")
+end
 
-Plug 'nvim-treesitter/nvim-treesitter'
+Plug('gelguy/wilder.nvim', { ['do'] = UpdateRemotePlugins })
 
-Plug 'neovim/nvim-lspconfig'
-Plug 'hrsh7th/cmp-nvim-lsp'
-Plug 'hrsh7th/cmp-buffer'
-Plug 'hrsh7th/cmp-path'
-Plug 'hrsh7th/cmp-cmdline'
-Plug 'hrsh7th/nvim-cmp'
+Plug('nvim-treesitter/nvim-treesitter')
 
-" For vsnip users.
-Plug 'hrsh7th/cmp-vsnip'
-Plug 'hrsh7th/vim-vsnip'
+Plug('neovim/nvim-lspconfig')
+Plug('hrsh7th/cmp-nvim-lsp')
+Plug('hrsh7th/cmp-buffer')
+Plug('hrsh7th/cmp-path')
+Plug('hrsh7th/cmp-cmdline')
+Plug('hrsh7th/nvim-cmp')
 
-lua << *
-	-- Set up nvim-cmp.
-  local cmp = require("nvim-cmp/plugin/cmp")
+-- For vsnip users.
+Plug('hrsh7th/cmp-vsnip')
+Plug('hrsh7th/vim-vsnip')
 
-  cmp.setup({
-    snippet = {
-      -- REQUIRED - you must specify a snippet engine
-      expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body) -- For vsnip users.
-        -- require('luasnip').lsp_expand(args.body) -- For luasnip users.
-        -- require('snippy').expand_snippet(args.body) -- For snippy users.
-        -- vim.fn["UltiSnips#Anon"](args.body) -- For ultisnips users.
-        -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
-      end,
-    },
-    window = {
-      -- completion = cmp.config.window.bordered(),
-      -- documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set select to false to only confirm explicitly selected items.
-    }),
-    sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      { name = 'vsnip' }, -- For vsnip users.
-      -- { name = 'luasnip' }, -- For luasnip users.
-      -- { name = 'ultisnips' }, -- For ultisnips users.
-      -- { name = 'snippy' }, -- For snippy users.
-    }, {
-      { name = 'buffer' },
-    })
+--package.path = "~/nvim/plugged/nvim-cmp"
+-- Set up nvim-cmp.
+local cmp = require("nvim-cmp")
+
+cmp.setup({
+snippet = {
+  -- REQUIRED - you must specify a snippet engine
+  expand = function(args)
+    vim.fn["vsnip#anonymous"](args.body) -- For vsnip users.
+    -- require('luasnip').lsp_expand(args.body) -- For luasnip users.
+    -- require('snippy').expand_snippet(args.body) -- For snippy users.
+    -- vim.fn["UltiSnips#Anon"](args.body) -- For ultisnips users.
+    -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+  end,
+},
+window = {
+  -- completion = cmp.config.window.bordered(),
+  -- documentation = cmp.config.window.bordered(),
+},
+mapping = cmp.mapping.preset.insert({
+  ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+  ['<C-f>'] = cmp.mapping.scroll_docs(4),
+  ['<C-Space>'] = cmp.mapping.complete(),
+  ['<C-e>'] = cmp.mapping.abort(),
+  ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set select to false to only confirm explicitly selected items.
+}),
+sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+    -- { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = 'buffer' },
   })
+})
 
-  -- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
-  -- Set configuration for specific filetype.
-  --[[ cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-      { name = 'git' },
-    }, {
-      { name = 'buffer' },
-    })
- })
- require("cmp_git").setup() ]]-- 
-
-  -- Use buffer source for / and ? (if you enabled native_menu, this won't work anymore).
-  cmp.setup.cmdline({ '/', '?' }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-      { name = 'buffer' }
-    }
+-- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
+-- Set configuration for specific filetype.
+--[[ cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    { name = 'git' },
+  }, {
+    { name = 'buffer' },
   })
+})
+require("cmp_git").setup() ]]-- 
 
-  -- Use cmdline & path source for ':' (if you enabled native_menu, this won't work anymore).
-  cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = 'path' }
-    }, {
-      { name = 'cmdline' }
-    }),
-    matching = { disallow_symbol_nonprefix_matching = false }
-  })
-
-  -- Set up lspconfig.
-  local capabilities = require('cmp_nvim_lsp').default_capabilities()
-  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-  require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
-    capabilities = capabilities
+-- Use buffer source for / and ? (if you enabled native_menu, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
   }
-*
+})
 
-call plug#end()
+-- Use cmdline & path source for ':' (if you enabled native_menu, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  }),
+  matching = { disallow_symbol_nonprefix_matching = false }
+})
+
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
+  capabilities = capabilities
+}
+
+require('latex').setup{
+  conceals = {
+    enabled = {
+      "greek",
+      "math",
+      "script",
+      "delim",
+      "font"
+    },
+    add = {}
+  },
+  imaps = {
+    enabled = true,
+    add = {},
+    default_leader = "\`"
+  },
+  surrounds = {
+    enabled = false,
+    command = "c",
+    environment = "e",
+  },
+}
+
+call("plug#end")
 EOF
 
-sed -i 's/*/EOF/g' init.vim
+sed -i 's/*/EOF/g' init.lua
 
 echo "Completed Install!"
